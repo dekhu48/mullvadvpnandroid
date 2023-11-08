mullvad/mullvadvpn-app

Add billing payment to welcome screen and view model

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/WelcomeDialogState.kt
@@ -1,0 +1,11 @@
+package net.mullvad.mullvadvpn.compose.state
+
+sealed interface WelcomeDialogState {
+    data object NoDialog: WelcomeDialogState
+
+    data object VerificationError: WelcomeDialogState
+
+    data object BillingError: WelcomeDialogState
+
+    data object PurchaseComplete: WelcomeDialogState
+}

dekhu48/mullvadvpnandroid

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/WelcomeDialogState.kt
@@ -1,0 +1,11 @@
+package net.mullvad.mullvadvpn.compose.state
+
+sealed interface WelcomeDialogState {
+    data object NoDialog : WelcomeDialogState
+
+    data object VerificationError : WelcomeDialogState
+
+    data object BillingError : WelcomeDialogState
+
+    data object PurchaseComplete : WelcomeDialogState
+}