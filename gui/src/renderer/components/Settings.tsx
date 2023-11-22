956e2590e63dc1c2cb5e1bf626e959d5cc678706
Oskar Nyberg <oskar@mullvad.net>
2023-11-22 13:36:41 +0100

    Add API access methods view

--- gui/src/renderer/components/Settings.tsx
+++ gui/src/renderer/components/Settings.tsx
@@ -72,6 +72,10 @@ export default function Support() {
                   )}
 
                   <Cell.Group>
+                    <ApiAccessMethodsButton />
+                  </Cell.Group>
+
+                  <Cell.Group>
                     <SupportButton />
                     <AppVersionButton />
                   </Cell.Group>
@@ -135,6 +139,22 @@ function SplitTunnelingButton() {
         {
           // TRANSLATORS: Navigation button to the 'Split tunneling' view
           messages.pgettext('settings-view', 'Split tunneling')
+        }
+      </Cell.Label>
+    </Cell.CellNavigationButton>
+  );
+}
+
+function ApiAccessMethodsButton() {
+  const history = useHistory();
+  const navigate = useCallback(() => history.push(RoutePath.apiAccessMethods), [history]);
+
+  return (
+    <Cell.CellNavigationButton onClick={navigate}>
+      <Cell.Label>
+        {
+          // TRANSLATORS: Navigation button to the 'API access methods' view
+          messages.pgettext('settings-view', 'API access methods')
         }
       </Cell.Label>
     </Cell.CellNavigationButton>
